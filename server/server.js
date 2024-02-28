a8d63b4bdaa43661ae5c2fc62f6ab8df8644b28a
TimiPashov <pashov91@gmail.com>
2024-02-28 12:17:27 +0200

    log when server starts

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('hello from server')
 })
 
 
 
-app.listen(3000);
+app.listen(3000, 'Listening on port 3000', () => { console.log('Listening on port 3000') });
